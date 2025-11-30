85f6fc605cabb2faf7719028675dfb5478ec195d
Sargeist <kukdgforgit@gmail.com>
2025-11-30 19:29:30 +0100

    Fix ASGI and WSGI pythonpath for Railway

--- Nezapadni/Nezapadni/asgi.py
+++ Nezapadni/Nezapadni/asgi.py
@@ -8,11 +8,17 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-
+import sys
+from pathlib import Path
 from django.core.asgi import get_asgi_application
 
+# Поднимаемся до корня проекта ("/app")
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+# Добавляем "/app" в PYTHONPATH
+if str(BASE_DIR) not in sys.path:
+    sys.path.insert(0, str(BASE_DIR))
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Nezapadni.settings')
-
-
 
 application = get_asgi_application()
--- Nezapadni/Nezapadni/wsgi.py
+++ Nezapadni/Nezapadni/wsgi.py
@@ -1,21 +1,14 @@
-"""
-WSGI config for Nezapadni project.
-"""
-
 import os
 import sys
 from pathlib import Path
 from django.core.wsgi import get_wsgi_application
 
-# BASE_DIR = /app/Nezapadni
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = /app
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-# Добавляем /app/Nezapadni в PYTHONPATH,
-# чтобы Django видел apps: accounts, main, courses
 if str(BASE_DIR) not in sys.path:
-    sys.path.append(str(BASE_DIR))
+    sys.path.insert(0, str(BASE_DIR))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Nezapadni.settings')
-
 
 application = get_wsgi_application()
