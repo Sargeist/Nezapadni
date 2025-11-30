7e27d514878f2d164f1ce84f97aa73e51dc5bae6
Sargeist <kukdgforgit@gmail.com>
2025-11-30 21:53:36 +0100

    Fix docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/sh
+set -e
 
 echo "Waiting for database..."
 sleep 3
@@ -7,4 +8,10 @@ echo "Running migrations..."
 python manage.py migrate --noinput
 
 echo "Starting Gunicorn..."
-gunicorn Nezapadni.wsgi:application --bind 0.0.0.0:${PORT}
+# Если Railway НЕ передал PORT → используй 8000
+PORT=${PORT:-8000}
+
+gunicorn Nezapadni.wsgi:application \
+    --bind 0.0.0.0:$PORT \
+    --workers 3 \
+    --log-level info
