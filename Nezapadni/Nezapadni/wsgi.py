8ae81106799d30cae3ca9958d9bbf8ff8bed9b43
Sargeist <kukdgforgit@gmail.com>
2025-11-30 16:06:47 +0100

    Fix PYTHONPATH for Railway (wsgi)

--- Nezapadni/Nezapadni/wsgi.py
+++ Nezapadni/Nezapadni/wsgi.py
@@ -1,17 +1,20 @@
 """
 WSGI config for Nezapadni project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
+from pathlib import Path
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Nezapadni.Nezapadni.settings')
+# BASE_DIR = /app/Nezapadni
+BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Добавляем /app/Nezapadni в PYTHONPATH,
+# чтобы Django видел apps: accounts, main, courses
+if str(BASE_DIR) not in sys.path:
+    sys.path.append(str(BASE_DIR))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Nezapadni.Nezapadni.settings")
 
 application = get_wsgi_application()
