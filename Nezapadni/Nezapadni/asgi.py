b34a4b50175cbcfe80b23b7e8450654fe65f48c4
Sargeist <kukdgforgit@gmail.com>
2025-11-30 20:01:56 +0100

    asgi update

--- Nezapadni/Nezapadni/asgi.py
+++ Nezapadni/Nezapadni/asgi.py
@@ -1,23 +1,9 @@
 """
 ASGI config for Nezapadni project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-import sys
-from pathlib import Path
 from django.core.asgi import get_asgi_application
-
-# Поднимаемся до корня проекта ("/app")
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
-# Добавляем "/app" в PYTHONPATH
-if str(BASE_DIR) not in sys.path:
-    sys.path.insert(0, str(BASE_DIR))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Nezapadni.settings')
 
